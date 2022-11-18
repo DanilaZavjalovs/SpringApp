e3cb8dd51e0d1759d6989114070280ee58d11b37
zloebok6 <54581698+zloebok6@users.noreply.github.com>
2022-11-18 16:20:49 +0200

    checkProfileWithID

--- ProfileSiteSpring/Spring-project/src/main/java/com/local/springproject/controllers/LoginController.java
+++ ProfileSiteSpring/Spring-project/src/main/java/com/local/springproject/controllers/LoginController.java
@@ -8,17 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
 @Slf4j
 @Controller
-@RequestMapping("/account")
+@RequestMapping("/authorization")
 public class LoginController {
     @Autowired
     UserRepository repository;
---
+++ ProfileSiteSpring/Spring-project/src/main/java/com/local/springproject/controllers/ProfileController.java
@@ -0,0 +1,36 @@
+package com.local.springproject.controllers;
+
+import com.local.springproject.JPA.UserRepository;
+import com.local.springproject.User.UserClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/account")
+public class ProfileController {
+    @Autowired
+    UserRepository repository;
+
+    @GetMapping("/profile/{id}")
+    public ModelAndView profile(@PathVariable("id") Integer id) {
+
+        ModelAndView view = new ModelAndView("home");
+
+        Optional<UserClass> user = repository.findById(id);
+
+        if (user.isPresent()) {
+            view.addObject("UserProfile", user.get());
+            view.setViewName("profile");
+
+            return view;
+        }
+
+        return view;
+    }
+}
